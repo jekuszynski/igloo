e8af9c2271b8f82034c28cda921bc4a316545fcb
jekuszynski <j.e.kuszynski@gmail.com>
2021-03-08 13:47:59 -0500

    Updated abs code

--- abs/cary50bio.py
+++ abs/cary50bio.py
@@ -4,8 +4,25 @@ import numpy as np
 import seaborn as sns
 
 directory = '/mnt/c/users/roflc/Desktop/Python Test Data/'
-filename = 'Nickel(II)Sulfate.csv'
+name='Nickel(II)Sulfate'
+filename = name + '.csv'
 filepath = directory + filename
 
-data=pd.read_csv(filepath)
-print(data.head(5))
+df=pd.read_csv(filepath,engine='python',skip_blank_lines=True,header=1,usecols=[0,1],skipfooter=48)
+# print(df)
+
+xlabel='Wavelength (nm)'
+ylabel='Abs'
+
+x=df[xlabel]
+y=df[ylabel]
+
+plt.plot(x,y,color='black')
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.xlim(275,1100)
+plt.ylim(0,1)
+plt.show()
+
+save_directory='/home/jkusz/github/igloo/abs/'
+plt.savefig(save_directory+name+'.png',dpi=300)
